03c0caec90e1ad42ecd1bb7ecdf1139114d63a06
fernando j barros <fer.j.doc@gmail.com>
2024-10-16 16:04:05 -0300

    fix: move data to config

--- Core/Config.js
+++ Core/Config.js
@@ -8,4 +8,9 @@ module.exports = {
     MIN_MAX_CHAR_CODE: 5,
     NAME: { MIN_CHAR: 5, MAX_CHAR: 120 },
   },
+  INSTALLMENTS: {
+    TOTAL_WITHOUT_INTEREST: 6,
+    DECIMAL_FIXED: 2,
+    INTEREST: 1.15,
+  },
 };
